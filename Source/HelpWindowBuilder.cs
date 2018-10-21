ba3276c4a8cc4427c4d823d88bfe00579a0e6f83
Jeffry Gaston <gastoj3@gmail.com>
2018-10-21 21:41:17 +0100

    Using the best font size for a help window based on what fits best

--- Source/HelpWindowBuilder.cs
+++ Source/HelpWindowBuilder.cs
@@ -20,13 +20,25 @@ namespace VisiPlacement
 
         public LayoutChoice_Set Build()
         {
-            GridLayout layout = GridLayout.New(new BoundProperty_List(this.messages.Count()), BoundProperty_List.Uniform(1), LayoutScore.Zero);
-            foreach (string message in this.messages)
+            List<LayoutChoice_Set> fontChoices = new List<LayoutChoice_Set>();
+            int fontSize;
+            for (fontSize = 20; fontSize >= 12; fontSize -= 8)
             {
-                string section = "    " + message;
-                layout.AddLayout(new TextblockLayout(section, 20));
+                GridLayout gridLayout = GridLayout.New(new BoundProperty_List(this.messages.Count()), BoundProperty_List.Uniform(1), LayoutScore.Zero);
+                foreach (string message in this.messages)
+                {
+                    string section = "    " + message;
+                    gridLayout.AddLayout(new TextblockLayout(section, fontSize));
+                }
+                fontChoices.Add(gridLayout);
             }
-            return new LayoutUnion(layout, ScrollLayout.New(layout));
+            List<LayoutChoice_Set> allChoices = new List<LayoutChoice_Set>();
+            foreach (LayoutChoice_Set fontChoice in fontChoices)
+            {
+                allChoices.Add(fontChoice);
+                allChoices.Add(ScrollLayout.New(fontChoice));
+            }
+            return new LayoutUnion(allChoices);
         }
 
         private LinkedList<string> messages = new LinkedList<string>();
