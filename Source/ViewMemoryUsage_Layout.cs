dcb58f92a8144e69b63f8b8cb43bdc1ae23a93a6
Jeffry Gaston <gastoj3@gmail.com>
2020-09-12 17:34:16 +0100

    A layout that displays memory usage

---
+++ Source/ViewMemoryUsage_Layout.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VisiPlacement;
+using Xamarin.Forms;
+
+namespace VisiPlacement
+{
+    public class ViewMemoryUsageLayout : ContainerLayout
+    {
+        public ViewMemoryUsageLayout()
+        {
+            this.SubLayout = this.textBlockLayout;
+        }
+
+        public override SpecificLayout GetBestLayout(LayoutQuery query)
+        {
+            long allocated = GC.GetTotalMemory(false);
+            // format a number like 1234567 into a string like 1,234,567
+            string formatted = String.Format("{0:#,0}", allocated);
+            this.textBlockLayout.setText("Memory usage: " + formatted + " bytes");
+            return base.GetBestLayout(query);
+        }
+
+        private TextblockLayout textBlockLayout = new TextblockLayout();
+    }
+}
